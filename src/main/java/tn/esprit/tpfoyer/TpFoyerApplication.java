e00087a267433be6a5f5c1716f34b0b9aeb7e08f
chaimaskouri <chaima.skouri@esprit.tn>
2024-10-28 17:29:27 +0100

    few changes in application.properties

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaRepositories

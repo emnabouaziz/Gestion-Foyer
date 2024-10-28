9656a789103d6102fde2bb4bab629255ee985809
MohamedHmaidi <hmaidi185@gmail.com>
2024-10-28 15:36:24 +0100

    add cross-org

--- src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
+++ src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
@@ -14,8 +14,9 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }

a36e8369c51c00f5c8212088d9da65df0df79093
MohamedHmaidi <hmaidi185@gmail.com>
2024-10-28 15:24:39 +0100

    add cross

--- src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/universite")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UniversiteRestController {
 
     IUniversiteService universiteService;

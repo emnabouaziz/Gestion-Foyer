ed717f33a0386d916351a23b907c8338c2f8d671
MohamedHmaidi <hmaidi185@gmail.com>
2024-10-29 11:50:13 +0100

    src

--- src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
+++ src/main/java/tn/esprit/tpfoyer/config/WebConfig.java
@@ -14,10 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://192.168.50.4")
+                        .allowedOriginPatterns("http://192.168.50.4:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
-                        .allowCredentials(false); // Make sure this is compatible with allowedOriginPatterns
+                        .allowCredentials(false);
             }
         };
     }
